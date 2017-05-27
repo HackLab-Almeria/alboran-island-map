ab71efdf3517ff1ae1c6fdcc778552e6ca213faf
gitolite <contacto@jesusmg.org>
2017-05-27 22:55:12 +0200

    Script funcionando...

--- script/generate_map_j.py
+++ script/generate_map_j.py
@@ -11,29 +11,66 @@ gdal.UseExceptions()
 import sqlite3
 import numpy as np
 
-blocks = {
-    '0,0,0' : 'default:stone',
-    '255,255,255' : 'default:dirt' 
-}
-
 def Usage():
     print('$ generate_map.py DEM_map features_map')
     sys.exit(1)
 
-def getBlockAsInteger(i, j, k):
-    return k*16777216 + j*4096 + i
+block_rgb_lookup = {
+    "Grass" : (35, 217, 72),
+    "Grass" : (158, 134, 26),
+    "Dirt" : (136, 40, 84),
+    "Cobblestone" : (220, 87, 237),
+    "StoneBricks" : (255, 255, 255),
+    "WoodPlanks" : (151, 110, 84),
+    "Obsidian" : (0, 0, 0),
+    "Gravel" : (193, 60, 151),
+}
+rgb_values = block_rgb_lookup.values()
 
-def getBlock(r, g, b):
-    rgb = '{0},{1},{2}'.format(r, g, b)
-    try:
-        return blocks[rgb]
-    except:
-        return blocks['0,0,0']
-
-def createBlob(block):
-    return '25,9,2,2,120,156,237,193,49,1,0,0,0,194,160,245,79,109,12,31,160,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,128,183,1,64,0,0,1,120,156,99,0,0,0,1,0,1,0,0,0,255,255,255,255,0,0,1,0,0,0,6,105,103,110,111,114,101,10,0,0'
+# R-values from the texture TIFF are converted to blocks of the given
+# blockID, blockData, depth.
+block_id_lookup = {
+    35 : (m.Grass.ID, None, 2),
+    158 : (m.Dirt.ID, 1, 1), # blockData 1 == grass can't spread
+    136 : (m.Grass.ID, None, 2),
+    220 : (m.Cobblestone.ID, None, 1),
+    255 : (m.StoneBricks.ID, None, 3),
+    151 : (m.WoodPlanks.ID, None, 2),
+    0  : (m.Obsidian.ID, None, 2),
+    193 : (m.Gravel.ID, None, 2),
+#    0   : (m.Water.ID, 0, 2), # blockData 0 == normal state of water
+#    220 : (m.WaterActive.ID, 0, 1),
+#    210 : (m.Water.ID, 0, 1),
+}
 
 # Fin definiciones
+
+def setspawnandsave(world, point):
+    """Sets the spawn point and player point in the world and saves the world.
+    Taken from TopoMC and tweaked to set biome.
+    """
+    world.GameType = 1
+    spawn = point
+    spawn[1] += 2
+    world.setPlayerPosition(tuple(point))
+    world.setPlayerSpawnPosition(tuple(spawn))
+
+    # In game mode, set the biome to Plains (1) so passive
+    #  mobs will spawn.
+    # In map mode, set the biome to Ocean (0) so they won't.
+    biome = 0
+    numchunks = 0
+    biomes = TAG_Byte_Array([biome] * 256, "Biomes")
+    for i, cPos in enumerate(world.allChunks, 1):
+        ch = world.getChunk(*cPos)
+        if ch.root_tag:
+            ch.root_tag['Level'].add(biomes)
+        numchunks += 1
+
+    world.saveInPlace()
+    print "Saved %d chunks." % numchunks
+
+# Fin funciones aux
 
 if len(sys.argv) < 3:
     print '[ ERROR ] you must supply two arguments: DEM_map features_map'
@@ -93,51 +130,99 @@ except RuntimeError, e:
 #    for j in range(0, len(dem_values[i])):
 #        print a
 
-if not os.path.exists("./minetest"):
-    os.makedirs("./minetest/")
+# Where does the world file go?
+minecraft_save_dir = './'
+y_min = 0
+elevation_min = 1
 
-with open("./minetest/world.mt", "w+") as wo:
-    wo.write("backend = sqlite3\n")
-    wo.write("gameid = minetest\n")
+i = 0
+worlddir = None
+while not worlddir or os.path.exists(worlddir):
+    i += 1
+    name = "world" + " " + str(i)
+    worlddir = os.path.join(minecraft_save_dir, name)
 
-if not os.path.exists("./minetest" + "/worldmods"):
-    os.makedirs("./minetest"+"/worldmods")
-if not os.path.exists("./minetest" + "/worldmods/mcimport"):
-    os.makedirs("./minetest"+"/worldmods/mcimport")
-if not os.path.exists("./minetest"+"/worldmods/mcimport/init.lua"):
-    with open("./minetest/worldmods/mcimport/init.lua", "w+") as sn:
-        sn.write("-- map conversion requires a special water level\n")
-        sn.write("minetest.set_mapgen_params({water_level = -2})\n\n")
-        sn.write("-- prevent overgeneration in incomplete chunks, and allow lbms to work\n")
-        sn.write("minetest.set_mapgen_params({chunksize = 1})\n\n")
-        sn.write("-- comment the line below if you want to enable mapgen (will destroy things!)\n")
-        sn.write("minetest.set_mapgen_params({mgname = \"singlenode\"})\n\n")
-        sn.write("-- below lines will recalculate lighting on map block load\n")
-        sn.write("minetest.register_on_generated(function(minp, maxp, seed)\n")
-        sn.write("        local vm = minetest.get_voxel_manip(minp, maxp)\n")
-        sn.write("        vm:set_lighting({day = 15, night = 0}, minp, maxp)\n")
-        sn.write("        vm:update_liquids()\n")
-        sn.write("        vm:write_to_map()\n")
-        sn.write("        vm:update_map()\n")
-        sn.write("end)\n\n")
+print "Creating world %s" % worlddir
+world = mclevel.MCInfdevOldLevel(worlddir, create=True)
+from pymclevel.nbt import TAG_Int, TAG_String, TAG_Byte_Array
+tags = [TAG_Int(0, "MapFeatures"),
+        TAG_String("flat", "generatorName"),
+        TAG_String("0", "generatorOptions")]
+for tag in tags:
+    world.root_tag['Data'].add(tag)
 
-mt_blocks = []
-int_blocks = []
-for i in range(0, len(dem_values)):
-    for j in range(0, len(dem_values[i])):
-       mt_blocks.append(getBlock(features_r[i][j], features_g[i][j], features_b[i][j]))
-       int_blocks.append(getBlockAsInteger(i,j,dem_values[i][j]))
+print "Creating chunks."
 
-conn = sqlite3.connect(os.path.join("./minetest/map.sqlite"))
-cur = conn.cursor()
+x_extent = HEIGHT
+x_min = 0
+x_max = HEIGHT
 
-cur.execute("CREATE TABLE IF NOT EXISTS `blocks` (\
-            `pos` INT NOT NULL PRIMARY KEY, `data` BLOB);")
+z_min = 0
+z_extent = WIDTH
+z_max = z_extent
 
-for i in range(0, len(mt_blocks)):
-    cur.execute("INSERT INTO blocks VALUES (?,?)",
-                        (int_blocks[i],
-                        createBlob(mt_blocks[i])))
-                    
-conn.commit()
-conn.close()
+extra_space = 1
+
+bedrock_bottom_left = [-extra_space, 0,-extra_space]
+bedrock_upper_right = [x_extent + extra_space + 1, y_min-1, z_extent + extra_space + 1]
+
+glass_bottom_left = list(bedrock_bottom_left)
+glass_bottom_left[1] += 1
+glass_upper_right = [x_extent + extra_space+1, 255, z_extent + extra_space+1]
+
+air_bottom_left = (0,y_min,0)
+air_upper_right = [x_extent, 255, z_extent]
+
+# Glass walls
+wall_material = m.Glass
+print "Putting up walls: %r %r" % (glass_bottom_left, glass_upper_right)
+tilebox = box.BoundingBox(glass_bottom_left, glass_upper_right)
+chunks = world.createChunksInBox(tilebox)
+world.fillBlocks(tilebox, wall_material)
+
+# Air in the middle.
+bottom_left = (0, 1, 0)
+upper_right = (HEIGHT, 255, WIDTH)
+print "Carving out air layer. %r %r" % (bottom_left, upper_right)
+tilebox = box.BoundingBox(bottom_left, upper_right)
+world.fillBlocks(tilebox, m.Air, [wall_material])
+
+max_height = (world.Height-elevation_min)
+
+print "Populating chunks."
+for i in range(HEIGHT):
+    for j in range(WIDTH):
+        block_id = features_r[i][j] #poner otros colores
+        try:
+            block_id, block_data, depth = block_id_lookup[block_id]
+
+        except KeyError, e:
+            block_id, block_data, depth = block_id_lookup[0]
+
+        y = dem_values[i][j]
+        actual_y = y + y_min
+
+        # Don't fill up the whole map from bedrock, just draw a shell.
+        start_at = max(1, actual_y-depth-10)
+
+        # If we were going to optimize this code, this is where the
+        # optimization would go. Lay down the stone in big slabs and
+        # then sprinkle goodies into it.
+        stop_at = actual_y-depth
+        for elev in range(start_at, stop_at):
+            world.setBlockAt(x,elev,z, block)
+
+        start_at = actual_y - depth
+        stop_at = actual_y + 1
+        if block_id == m.WaterActive.ID:
+            # Carve a little channel for active water so it doesn't overflow.
+            start_at -= 1
+            stop_at -= 1
+        for elev in range(start_at, stop_at):
+            world.setBlockAt(i, elev, j, block_id)
+            if block_data:
+                world.setBlockDataAt(i, elev, j, block_data)
+        break
+
+peak = [10, 255, 10]
+setspawnandsave(world, peak)
